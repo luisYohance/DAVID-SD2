efbf2e5a9dc7f07c3d304c325af6409aed5746c1
luisYohance <luisyohancedavid@gmail.com>
2026-04-21 17:27:47 +0800

    Reverted RPS logic

--- midtermExam/index.js
+++ midtermExam/index.js
@@ -52,7 +52,7 @@ async function evaluateResult() {
       console.log(`Player (${choices[choice]}) won!`);
       playerScore++;
       // Win condition : Player won because their choice is greater than 1
-    } else if ((cpuChoice - 1) % 3 == choice) {
+    } else if (cpuChoice != choice) {
       evalAudio = new Audio("sfx/lose.mp3");
       console.log(`CPU (${choices[cpuChoice]}) won!`);
       cpuScore++;
